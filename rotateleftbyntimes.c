48d57f7c7c692c6d394f90facab911e02c363dbc
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-03-02 09:19:27 +0530

    Update rotateleftbyntimes.c

--- rotateleftbyntimes.c
+++ rotateleftbyntimes.c
@@ -3,20 +3,27 @@
 
 int main()
 {
-    long int num,i=0,j,nooftimes,arr[100],pos,temp;
+    long int num,length,j,nooftimes,arr[100],pos,temp,temp1,c=0;
     scanf("%ld\n%ld",&num,&nooftimes);
-    temp=num;
+    temp=num;temp1=num;
+    while(temp1>0)
+    {
+        c++;
+        temp1=temp1/10;
+    } 
+    length=c;
+    c--;
     while(temp>0)
     {
-        arr[i++]=temp%10;
+        arr[c--]=temp%10;
         temp=temp/10;
     }
-    pos=nooftimes%i;
-    for(j=pos-1;j>=0;j--)
+    pos=nooftimes%length;
+    for(j=pos;j<length;j++)
     {
         printf("%ld",arr[j]);
     }
-     for(j=pos;j<i;j++)
+    for(j=0;j<pos;j++)
     {
         printf("%ld",arr[j]);
     }
