1aa495ee9ad000264ea12e34de7f2cdce01ea573
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-03-03 22:57:58 -0800

    Update noofoccur.c

--- noofoccur.c
+++ noofoccur.c
@@ -7,3 +7,30 @@ Output
 0 --> 1 
 1 --> 2 
 5 --> 1*/
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[1000],i,k=0,j,count=1;
+    while(scanf("%d",&a[k])>=0)
+    {
+        k++;
+    }
+    for(i=0;i<k;i++)
+    {
+        if(a[i]>=0)
+        {
+            for(j=i+1;j<k;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    count++;
+                    a[j]=-1;
+                }
+            }
+            printf("%d-->%d\n",a[i],count);
+            count=1;
+        }
+    }
+}
