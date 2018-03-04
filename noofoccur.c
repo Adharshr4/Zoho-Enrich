512fddf3b7626b3504b4c590623945efac1f6836
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-03-03 22:55:37 -0800

    Create noofoccur.c

---
+++ noofoccur.c
@@ -0,0 +1,9 @@
+/*Write a program to print the number of occurrences of each number in a given array that contains the positive numbers 
+Ex: 
+Input 
+Array : [2, 0, 1, 2, 5, 1] 
+Output 
+2 --> 2 
+0 --> 1 
+1 --> 2 
+5 --> 1*/
