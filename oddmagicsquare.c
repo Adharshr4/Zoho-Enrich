36c21f811e7c6fa4d8746010137b07662f33de77
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-03-16 06:28:55 -0700

    Update oddmagicsquare.c

--- oddmagicsquare.c
+++ oddmagicsquare.c
@@ -15,30 +15,38 @@ int main()
 {
     int n,i=0,k=2,p,rowc,colc;
     scanf("%d",&n);
-    int a[100][100]={0},j=n/2;
-    a[i][j]=1;
-    while(k<=n*n)
+    if(n%2==0)
     {
-        rowc=(i-1+n)%n;
-        colc=(j+1+n)%n;
-        if(a[rowc][colc]==0)
-        {
-            a[rowc][colc]=k++;
-            i=rowc;
-            j=colc;
-        }
-        else
-        {
-            i++;
-            a[i][j]=k++;
-        }
+        printf("n is even so terminated");
+        return 0;
     }
-    for(i=0;i<n;i++)
+    else
     {
-       for(j=0;j<n;j++)
-       {
-            printf("%d\t",a[i][j]);
-       }
-        printf("\n");
+        int a[100][100]={0},j=n/2;
+        a[i][j]=1;
+        while(k<=n*n)
+        {
+            rowc=(i-1+n)%n;
+            colc=(j+1+n)%n;
+            if(a[rowc][colc]==0)
+            {
+                a[rowc][colc]=k++;
+                i=rowc;
+                j=colc;
+            }
+            else
+            {
+                i++;
+                a[i][j]=k++;
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                printf("%d\t",a[i][j]);
+            }
+            printf("\n");
+        }
     }
 }
