c42679ac096bcd717136831c6c775448467d2496
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-03-20 05:21:38 -0700

    Update decimalToAnyBase.c

--- decimalToAnyBase.c
+++ decimalToAnyBase.c
@@ -4,6 +4,11 @@ int main()
 {
     int i=0,num,base,a[1000],dupnum,k=0;
     scanf("%d %d",&num,&base);
+    if(num==0)
+    {
+        printf("Enter a postive number");
+        return 0;
+    }
     dupnum=num;
     while(dupnum>0)
     {
