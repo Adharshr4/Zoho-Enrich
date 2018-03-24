01ebd17dcbf5b3bde55ae219b9d6c4b63fb40ef7
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-03-24 08:00:10 -0700

    Update sortstringnum.c

--- sortstringnum.c
+++ sortstringnum.c
@@ -3,32 +3,26 @@
 
 int main()
 {
-    char a[100][100];
-    int i,j,k=0,arr[100],t,sum,l;
+    char a[100][100],temp[100];
+    int i,k=0,j;
     while(scanf("%s",a[k])>0)
     {
-        sum=a[k][0]-'0';
-        for(i=0;i<strlen(a[k])-1;i++)
-        {
-            sum=sum*10+(a[k][i+1]-'0');
-        }
-        arr[k]=sum;
         k++;
     }
     for(i=0;i<k;i++)
     {
         for(j=i+1;j<k;j++)
         {
-            if(arr[i]>arr[j])
+            if ( (strcmp(a[i],a[j])>0 && strlen(a[i])==strlen(a[j]) )  || (strlen(a[i])>strlen(a[j]) ) )
             {
-                t=arr[i];
-                arr[i]=arr[j];
-                arr[j]=t;
+                strcpy(temp,a[i]);
+                strcpy(a[i],a[j]);
+                strcpy(a[j],temp);
             }
         }
     }
     for(i=0;i<k;i++)
     {
-        printf("%d ",arr[i]);
+        printf("%s ",a[i]);
     }
 }
